b2ae86c11d3b9b5dc361e6ba7958aa7be50cd7c9
ArcDemon7 <93246120+ArcDemon7@users.noreply.github.com>
2026-03-24 22:03:30 +1030

    added source folder

---
+++ game/Source/Jenkins_Project/Jenkins_Project.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Jenkins_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Jenkins_Project, "Jenkins_Project" );
